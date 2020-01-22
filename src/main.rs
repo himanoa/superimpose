08d451bf4472d8be13fa8cb9ad6886de70fe2698
himanoa <matsunoappy@gmail.com>
2020-01-22 10:58:55 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{App, Arg};
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("superimpose")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Superimpose is snapshot test helper for command execution result.")
+        .arg(
+            Arg::with_name("command")
+                .help("snapshot target command")
+                .empty_values(false)
+                .multiple(true),
+        ).get_matches();
+    let command: Vec<_> = matches.values_of("command").unwrap().collect();
+    println!("{:?}", command);
 }
