b9fefd2154e9dfed5944f69dc474654db06b5fde
himanoa <matsunoappy@gmail.com>
2020-01-23 01:48:28 +0900

    Support snapshot key

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ fn main() {
     let matches = App::new("superimpose")
         .version(env!("CARGO_PKG_VERSION"))
         .about("Superimpose is snapshot test helper for command execution result.")
+        .arg(Arg::with_name("key").help("snapshot key").takes_value(true).required(true))
         .arg(
             Arg::with_name("command")
                 .help("snapshot target command")
                 .empty_values(false)
                 .multiple(true),
-        ).get_matches();
+        )
+        .get_matches();
     let command: Vec<_> = matches.values_of("command").unwrap().collect();
-    println!("{:?}", command);
+    let snapshot_key: String = matches.values_of("key").unwrap().collect();
 }
